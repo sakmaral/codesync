38de1212a20109cc3cad022d968ce801a1dad555
qmrls <sakmaral703@gmail.com>
2024-10-21 16:00:10 +0500

    feat: add editor library and tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react';
+import { Editor } from '@monaco-editor/react';
+import { useEffect } from 'react';
 import io from 'socket.io-client';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const socket = io('http://localhost:3000');
 
   useEffect(() => {
@@ -25,24 +21,9 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="h-screen w-screen">
+      <Editor height="100vh" defaultLanguage="javascript" defaultValue="// some comment" />
+    </div>
   );
 }
 
